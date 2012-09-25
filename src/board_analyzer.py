144910516074be3b9c1b1c42402bf9165973d6dc
Adam Gooch <adamgooch@acm.org>
2012-09-25 17:53:35 -0400

    Game is playable with 2 human players

--- src/board_analyzer.py
+++ src/board_analyzer.py
@@ -1,9 +1,28 @@
 class BoardAnalyzer:
 
+  win_sets = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+              [0, 3, 6], [1, 4, 7], [2, 5, 8],
+              [0, 4, 8], [2, 4, 6]]
+
   def game_over(self, board):
-    if self.open_squares(board) == 0:
+    if self.winner('X', board):
+      return True
+    elif self.winner('O', board):
+      return True
+    elif self.open_squares(board) == 0:
       self.winner = 'Nobody'
       return True
+    return False
+
+  def winner(self, player, board):
+    for win_set in self.win_sets:
+      count = 0
+      for square in win_set:
+        if board[square] == player:
+          count += 1
+      if count == 3:
+        self.winner = player
+        return True
 
   def open_squares(self, board):
     open_squares = 0
--- tests/test_board_analyzer.py
+++ tests/test_board_analyzer.py
@@ -4,13 +4,34 @@ from src.board_analyzer import BoardAnalyzer
 
 class TestBoardAnalyzer(unittest.TestCase):
 
-  def test_game_over_is_true_when_board_is_full(self):
-    board = [ 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O' ]
-    board_analyzer = BoardAnalyzer()
-    self.assertEqual(True, board_analyzer.game_over(board))
+  no_winner_board = [ 'X', 'O', 'X', 'O', 'O', 'X', 'X', 'X', 'O' ]
+  empty_board = [ '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
+  x_wins_board = [ 'X', 'X', 'X', 'O', '5', 'O', '7', 'O', '9' ]
+  o_wins_board = [ 'O', 'O', 'O', 'X', '5', 'X', '7', 'X', '9' ]
 
-  def test_game_over_sets_the_winner_value(self):
-    board = [ 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O' ]
-    board_analyzer = BoardAnalyzer()
-    board_analyzer.game_over(board)
-    self.assertEqual('Nobody', board_analyzer.winner)
+  def setUp(self):
+    self.board_analyzer = BoardAnalyzer()
+
+  def test_game_over_is_true_when_board_is_full(self):
+    self.assertTrue(self.board_analyzer.game_over(self.no_winner_board))
+
+  def test_game_over_is_false_when_there_is_no_winner_and_board_is_not_full(self):
+    self.assertFalse(self.board_analyzer.game_over(self.empty_board))
+
+  def test_game_over_is_true_when_x_has_won(self):
+    self.assertTrue(self.board_analyzer.game_over(self.x_wins_board))
+
+  def test_game_over_is_true_when_o_has_won(self):
+    self.assertTrue(self.board_analyzer.game_over(self.o_wins_board))
+
+  def test_game_over_sets_the_winner_value_to_nobody(self):
+    self.board_analyzer.game_over(self.no_winner_board)
+    self.assertEqual('Nobody', self.board_analyzer.winner)
+
+  def test_game_over_sets_the_winner_to_x(self):
+    self.board_analyzer.game_over(self.x_wins_board)
+    self.assertEqual('X', self.board_analyzer.winner)
+
+  def test_game_over_sets_the_winner_to_o(self):
+    self.board_analyzer.game_over(self.o_wins_board)
+    self.assertEqual('O', self.board_analyzer.winner)
